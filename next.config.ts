5fc70e823e343b0b33f0dbf7954ffdf276c1e727
Amit Mittal <ankit.sharma@routadigital.com>
2026-02-25 00:24:03 +0530

    Build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone', // ✅ standalone build for SSR
-  reactStrictMode: true,
+  /* config options here */
 };
 
 export default nextConfig;
